23d1feee9fad6983442b74efd6af7de51c9a0906
sheready <csstlw@gmail.com>
2020-05-19 18:26:53 +0300

    add toggle and alert methods

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $(".we").click(function(){
+        $(this).children(".hide").slideToggle(1500);
+        $(this).children(".show").toggle(1500);
+    });
+    $(".slide").hover( function() {
+        $(this).find(".img-title").fadeIn(300);
+    }, function() {
+        $(this).find(".img-title").fadeOut(100);
+    });
+    $("#submit").click(function(close){
+        var name = $("#b_name").val();
+        alert(name + " we have received your message. Thank you for reaching out to us.")
+    });
+   
+    });
